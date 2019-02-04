ac390234cec90b6d17a80fe120d5969f72ac43de
D Vargas <hiphipvargas@sapo.pt>
2019-02-04 20:23:08 +0100

    OPENEUROPA-1552: Add main middleware for proxyticket.

---
+++ src/Middleware/CasProxyTicketMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace OpenEuropa\EPoetry\Middleware;
+
+use Http\Promise\Promise;
+use Phpro\SoapClient\Middleware\Middleware;
+use Phpro\SoapClient\Middleware\MiddlewareInterface;
+use Psr\Http\Message\RequestInterface;
+
+class CasProxyTicketMiddleware extends Middleware implements MiddlewareInterface
+{
+    /**
+     * The Proxy Ticket to add to requests.
+     *
+     * @var string
+     */
+    protected $proxyTicket;
+
+    /**
+     * CasProxyTicketMiddleware constructor.
+     *
+     * @param string $proxyTicket
+     */
+    public function __construct(string $proxyTicket)
+    {
+        $this->proxyTicket = $proxyTicket;
+    }
+
+    public function beforeRequest(callable $handler, RequestInterface $request): Promise
+    {
+        // Add Proxy Ticket.
+        $request = $request->withHeader('ecas:ProxyTicket', $this->proxyTicket);
+
+        return $handler($request);
+    }
+
+    public function getName(): string
+    {
+        return 'cas_proxy_ticket_middleware';
+    }
+}
--- tests/Middleware/MiddlewareTest.php
+++ tests/Middleware/MiddlewareTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace OpenEuropa\EPoetry\Tests\Middleware;
 
 use GuzzleHttp\Psr7\Response;
+use OpenEuropa\EPoetry\Middleware\CasProxyTicketMiddleware;
 use OpenEuropa\EPoetry\Tests\AbstractTest;
 use OpenEuropa\EPoetry\Type\CreateRequests;
 use OpenEuropa\EPoetry\Type\LinguisticRequestIn;
@@ -17,9 +18,38 @@ use OpenEuropa\EPoetry\Type\RequestGeneralInfoIn;
 final class MiddlewareTest extends AbstractTest
 {
     /**
-     * Test Proxy Ticket in request.
+     * Test Proxy Ticket in request using main middleware.
      */
-    public function testResponseParsing()
+    public function testProxyTicketFromMainMiddleware()
+    {
+        // Generate request.
+        $generalInfo = new RequestGeneralInfoIn();
+        $generalInfo->setTitle('Test');
+        $linguisticRequestIn = new LinguisticRequestIn();
+        $linguisticRequestIn->setGeneralInfo($generalInfo);
+        $createRequests = new CreateRequests();
+        $createRequests->setLinguisticRequest($linguisticRequestIn);
+
+        // Generate response.
+        $content = file_get_contents(self::FIXTURE_DIR . '/create-requests-response.xml');
+        $response = new Response(200, [], $content);
+        $this->httpClient->addResponse($response);
+
+        $clientFactory = $this->createClientFactory();
+        $clientFactory->addMiddleware(new CasProxyTicketMiddleware('DESKTOP_PT-21-9fp9s'));
+        $client = $clientFactory->getClient();
+        $client->createRequests($createRequests);
+
+        $request = $client->debugLastSoapRequest()['request'];
+
+        $this->assertContains('<title>Test</title>', $request['body']);
+        $this->assertContains('ecas:ProxyTicket: DESKTOP_PT-21-9fp9', $request['headers'], 'Request XML header malformed, missing proxy ticket.');
+    }
+
+    /**
+     * Test Proxy Ticket in request using test middleware.
+     */
+    public function testProxyTicketFromTestMiddleware()
     {
         // Generate request.
         $generalInfo = new RequestGeneralInfoIn();
