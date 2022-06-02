8980c9785b3af615732839541e89ead1ba7eed1d
Antonio De Marco <antonio@nuvole.org>
2022-06-02 10:58:25 +0200

    EWPP-2281: Add console scaffolding and fix deps.

---
+++ src/Exception/ClientException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace OpenEuropa\EPoetry\Exception;
+
+/**
+ * Generic ePoetry exception, all client exceptions extend this.
+ */
+class ClientException extends \Exception
+{
+}
