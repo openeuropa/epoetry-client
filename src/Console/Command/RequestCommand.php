d13da2b0c003f98a0614e2c1a2bcb3734a19e7bc
Antonio De Marco <antonio@nuvole.org>
2022-09-30 13:39:18 +0200

    EWPP-2611: Wire container on Symfony command.

---
+++ src/Console/Command/RequestCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace OpenEuropa\EPoetry\Console\Command;
+
+use Monolog\Logger;
+use OpenEuropa\EPoetry\RequestClientFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
+
+class RequestCommand extends Command
+{
+    protected static $defaultName = 'request';
+
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        parent::__construct(null);
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setDescription('Build and send a CreateRequests to ePoetry.')
+            ->addArgument('endpoint', InputArgument::REQUIRED, 'ePoetry service endpoint')
+            ->addArgument('ticket', InputArgument::REQUIRED, 'Authentication ticket')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        //        /** @var $logger LoggerInterface */
+        //        $logger = $this->getContainer()->get('logger');
+
+        $client = new RequestClientFactory(
+            $input->getArgument('endpoint'),
+            $input->getArgument('ticket')
+        );
+
+        return 0;
+    }
+}
