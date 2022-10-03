69bd30a3eefd5d95412b42aaaefd83501a4d5e2d
Antonio De Marco <antonio@nuvole.org>
2022-10-03 14:58:58 +0200

    EWPP-2611: Add authentication.

--- src/Console/Command/RequestCommand.php
+++ src/Console/Command/RequestCommand.php
@@ -22,9 +22,14 @@ use OpenEuropa\EPoetry\Serializer\Serializer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Facile\OpenIDClient\Client\ClientBuilder;
+use Facile\OpenIDClient\Issuer\IssuerBuilder;
+use Facile\OpenIDClient\Client\Metadata\ClientMetadata;
+use Facile\OpenIDClient\Service\Builder\AuthorizationServiceBuilder;
 
 class RequestCommand extends Command
 {
@@ -47,9 +52,7 @@ class RequestCommand extends Command
     protected function configure()
     {
         $this->setDescription('Build and send a CreateRequests to ePoetry.')
-            ->addArgument('endpoint', InputArgument::REQUIRED, 'ePoetry service endpoint')
-            ->addArgument('ticket', InputArgument::REQUIRED, 'Authentication ticket')
-        ;
+            ->addOption('endpoint', 'e', InputOption::VALUE_REQUIRED, 'ePoetry service endpoint', 'https://webgate.acceptance.ec.europa.eu/epoetrytst/epoetry/webservices/dgtService');
     }
 
     /**
@@ -57,7 +60,8 @@ class RequestCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $factory = new RequestClientFactory($input->getArgument('endpoint'), $input->getArgument('ticket'), null, $this->logger);
+        $ticket = $this->getTicket();
+        $factory = new RequestClientFactory($input->getOption('endpoint'), $ticket, null, $this->logger);
         $this->logger->info('Endpoint: ' . $factory->getEndpoint());
         $this->logger->info('Proxy ticket: ' . $factory->getProxyTicket());
         $client = $factory->getRequestClient();
@@ -105,8 +109,8 @@ class RequestCommand extends Command
             ->setKeyword3('keyword3')
             ->setAuxiliaryDocuments($auxiliaryDocuments);
         $contacts = (new Contacts())
-            ->addContact(new ContactPersonIn('liekejo', 'REQUESTER'))
-            ->addContact(new ContactPersonIn('liekejo', 'AUTHOR'))
+            ->addContact(new ContactPersonIn('liekejo', 'WEBMASTER'))
+            ->addContact(new ContactPersonIn('liekejo', 'EDITOR'))
             ->addContact(new ContactPersonIn('liekejo', 'RECIPIENT'));
         $requestDetails->setContacts($contacts);
 
@@ -130,7 +134,28 @@ class RequestCommand extends Command
 
         return (new CreateLinguisticRequest())
             ->setRequestDetails($requestDetails)
-            ->setApplicationName('appname')
-            ->setTemplateName('DEFAULT');
+            ->setApplicationName('DRUPAL-TEST')
+            ->setTemplateName('WEBTRA');
+    }
+
+    protected function getTicket(): string
+    {
+        $issuer = (new IssuerBuilder())->build('https://ecas.acceptance.ec.europa.eu/cas/oauth2/.well-known/openid-configuration');
+        $metadata = json_decode(file_get_contents(__DIR__ . '/../../../client-metadata.json'), true);
+        $clientMetadata = ClientMetadata::fromArray($metadata);
+        $client = (new ClientBuilder())
+            ->setIssuer($issuer)
+            ->setClientMetadata($clientMetadata)
+            ->build();
+
+        $authorizationService = (new AuthorizationServiceBuilder())->build();
+        $tokenSet = $authorizationService->grant($client, [
+            "grant_type" => "client_credentials",
+            "resource" => "https://www.test.cc.cec/epoetry/webservices/dgtService",
+            "aud" => "https://ecas.acceptance.ec.europa.eu/cas/oauth2/token",
+            "requested_token_type" => "urn:ietf:params:oauth:token-type:cas_ticket",
+        ]);
+
+        return $tokenSet->getAccessToken();
     }
 }
