0fa8f75a80ea6722a8fd590e6f698d3131d5a6d5
Antonio De Marco <antonio@nuvole.org>
2023-04-06 10:27:08 +0200

    EWPP-3242: Do not extend event with deadline.

--- src/Notification/Event/Product/StatusChangeRejectedEvent.php
+++ src/Notification/Event/Product/StatusChangeRejectedEvent.php
@@ -5,7 +5,7 @@ namespace OpenEuropa\EPoetry\Notification\Event\Product;
 /**
  * Event fired when the status of the product changes to "Rejected".
  */
-class StatusChangeRejectedEvent extends BaseEventWithDeadline
+class StatusChangeRejectedEvent extends BaseEvent
 {
     public const NAME = 'epoetry.notification.product_status_change_rejected';
 }
