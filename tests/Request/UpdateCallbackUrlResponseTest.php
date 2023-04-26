4e03d8a3dab1fb011361952363c0a871a182f03a
Antonio De Marco <antonio@nuvole.org>
2023-04-26 12:02:25 +0200

    EWPP-3245: Fix schema and add test.

--- tests/Request/UpdateCallbackUrlResponseTest.php
+++ tests/Request/UpdateCallbackUrlResponseTest.php
@@ -4,6 +4,11 @@ declare(strict_types = 1);
 
 namespace OpenEuropa\EPoetry\Tests\Request;
 
+use Http\Mock\Client as MockClient;
+use Nyholm\Psr7\Response;
+use OpenEuropa\EPoetry\Request\Type\UpdateCallbackUrl;
+use OpenEuropa\EPoetry\RequestClientFactory;
+use OpenEuropa\EPoetry\Tests\Authentication\MockAuthentication;
 use Soap\Engine\HttpBinding\SoapResponse;
 
 /**
@@ -20,6 +25,50 @@ final class UpdateCallbackUrlResponseTest extends BaseRequestTest
     {
         $object = $this->serializer->deserialize($response, 'OpenEuropa\EPoetry\Request\Type\UpdateCallbackUrlResponse', 'xml');
         $this->assertExpressionLanguageExpressions($expectations['assertions'], ['response' => $object]);
+    }
+
+    /**
+     * Tests raw response processing.
+     */
+    public function testUpdateCallbackUrlRawResponse(): void
+    {
+        $mockClient = new MockClient();
+        $authentication = new MockAuthentication('[proxy ticket]');
+
+        $response = <<<RESPONSE
+<?xml version='1.0' encoding='UTF-8'?>
+<S:Envelope xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:S="http://schemas.xmlsoap.org/soap/envelope/">
+  <env:Header/>
+  <S:Body>
+    <ns0:updateCallbackUrlResponse xmlns:ns0="http://eu.europa.ec.dgt.epoetry">
+      <return>
+        <success>true</success>
+        <oldCallbackUrl>https://old.url</oldCallbackUrl>
+        <newCallbackUrl>https://new.url</newCallbackUrl>
+        <application>app</application>
+        <message>Callback successfully updated</message>
+      </return>
+    </ns0:updateCallbackUrlResponse>
+  </S:Body>
+</S:Envelope>
+RESPONSE;
+
+
+        // Assert proxy token exists in the header.
+        $mockClient->addResponse(new Response(200, [], $response));
+        $clientFactory = new RequestClientFactory('http://foo.bar', $authentication, null, null, $mockClient);
+        $requestClient = $clientFactory->getRequestClient();
+
+        $request = (new UpdateCallbackUrl())
+            ->setCallbackUrl('https://webgate.ec.europa.eu/poetryresponse/epoetry-test.fpfis.eu/')
+            ->setApplicationName('DRUPAL-TEST');
+
+        $response = $requestClient->updateCallbackUrl($request);
+        $this->assertEquals('https://old.url', $response->getReturn()->getOldCallbackUrl());
+        $this->assertEquals('https://new.url', $response->getReturn()->getNewCallbackUrl());
+        $this->assertEquals('app', $response->getReturn()->getApplication());
+        $this->assertEquals('Callback successfully updated', $response->getReturn()->getMessage());
+        $this->assertEquals(true, $response->getReturn()->isSuccess());
     }
 
     /**
